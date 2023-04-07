43bbeab04ba1d5f96719f767b4030503299a8fd5
Kirill Gorbunov <4pduck@gmail.com>
2023-04-07 15:24:56 +0300

    added fixes

--- Homework 2.2.1/main.cpp
+++ Homework 2.2.1/main.cpp
@@ -3,6 +3,7 @@
 
 enum class seasons
 {
+	выход,
 	€нварь = 1,
 	февраль,
 	март,
@@ -15,6 +16,7 @@ enum class seasons
 	окт€брь,
 	но€брь,
 	декабрь
+
 };
 
 
@@ -23,58 +25,59 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 
 	int p;
+	seasons s;
 
 	static_cast<seasons>(p);
 	do {
 		std::cin >> p;
 
-		static_cast<seasons>(p);
+		s=static_cast<seasons>(p);
 
-		switch (p) {
-		case(static_cast<int>(seasons::€нварь)):
+		switch (s) {
+		case(seasons::€нварь):
 			std::cout << "январь" << std::endl;
 			break;
-		case(static_cast<int>(seasons::февраль)):
+		case(seasons::февраль):
 			std::cout << "‘евраль" << std::endl;
 			break;
-		case(static_cast<int>(seasons::март)):
+		case(seasons::март):
 			std::cout << "ћарт" << std::endl;
 			break;
-		case(static_cast<int>(seasons::апрель)):
+		case(seasons::апрель):
 			std::cout << "јпрель" << std::endl;
 			break;
-		case(static_cast<int>(seasons::май)):
+		case(seasons::май):
 			std::cout << "ћай" << std::endl;
 			break;
-		case(static_cast<int>(seasons::июнь)):
+		case(seasons::июнь):
 			std::cout << "»юнь" << std::endl;
 			break;
-		case(static_cast<int>(seasons::июль)):
+		case(seasons::июль):
 			std::cout << "»юль" << std::endl;
 			break;
-		case(static_cast<int>(seasons::август)):
+		case(seasons::август):
 			std::cout << "јвгуст" << std::endl;
 			break;
-		case(static_cast<int>(seasons::сент€брь)):
+		case(seasons::сент€брь):
 			std::cout << "—ент€брь" << std::endl;
 			break;
-		case(static_cast<int>(seasons::окт€брь)):
+		case(seasons::окт€брь):
 			std::cout << "ќкт€брь" << std::endl;
 			break;
-		case(static_cast<int>(seasons::но€брь)):
+		case(seasons::но€брь):
 			std::cout << "Ќо€брь" << std::endl;
 			break;
-		case(static_cast<int>(seasons::декабрь)):
+		case(seasons::декабрь):
 			std::cout << "ƒекабрь" << std::endl;
 			break;
-		case(0):
+		case(seasons::выход):
 			std::cout <<"ƒо свидани€" << std::endl;
 			break;
 
 		default:
 			std::cout << "Ќеправильный номер!" << std::endl;
 		}
-	} while (p != 0);
+	} while (s != seasons::выход);
 
 	return 0;
 
